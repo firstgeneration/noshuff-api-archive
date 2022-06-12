b33f3a21ddc85c4ce1663e6b2a777e45f3235d58
Daniel Gantz <daniel.gantz@gmail.com>
2022-06-12 18:11:31 -0400

    user model + migration

--- core/models.py
+++ core/models.py
@@ -3,4 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    id = models.CharField(max_length=100, primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    display_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    avatar_url = models.URLField(null=True)
